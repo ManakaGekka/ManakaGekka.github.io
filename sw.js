0c824fd243c70cb5d4d54ad743baaf530a1b229b
ManakaGekka <651715794@qq.com>
2024-11-24 03:27:54 +0800

    Site updated: 2024-11-24 03:27:47

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732389767649";
+const VERSION = "1732390032886";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
